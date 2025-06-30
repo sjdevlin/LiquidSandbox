702c10d04185622acbdd36f7fdc55fd7bd77180a
Stephen Devlin <sjdevlin@gmail.com>
2025-06-30 11:40:39 +0100

    minor changes to image run operator

--- operators/image_run_operator.py
+++ operators/image_run_operator.py
@@ -22,13 +22,52 @@ class ImageRunOperator:
 
 
     def run(self):
-        # Calculate experiment duration and build per-minute target temperature profile
+        from views import LogView
+#        import threading
+        from tkinter import messagebox
 
         self.start_date_time = datetime.now()
+        self.status = "Not Started"
+
+        # ask user to ensure that image is in focus before starting the run
+        self.logger.info("Please ensure that the image is in focus before starting the run.")   
+        # create window that pauses the run until the user clicks "Continue"
+        messagebox.showinfo("Focus Check", "Please ensure that the image is in focus before starting the run. Click 'Continue' to proceed.")  
+        
+        log_window = LogView(self.view.root_window, self.logger.log_file)
+        self.focus_controller.autofocus(True)  # Ensure autofocus is off before starting the run
+
+       # First create the image run in the database, then retrieve it.  
+        # This ensures that the image run is created before we start the imaging process.
+        self.db.add_image_run(ImageRun(
+            image_set_id=self.image_set.id,
+            experiment_id=self.experiment.id,
+            description= self.image_set.description,
+            notes=(f"Experiment: {self.experiment.description}\nImage Set: {self.image_set.description}")
+            image_set_start_date_time=self.start_date_time,
+            image_set_status=self.status
+        ))
+
+        self.experiment.image_run = self.db.get_image_run_by_id(self.image_set.id, self.experiment.id)
+        # Iterate through each sample in the experiment
+        self.experiment.status = "Running"
+        self.db.update_image_run(self.experiment)   
+        self.logger.info(f"Started imaging run for experiment {self.experiment.id} with image set {self.image_set.id}")
+
+        #home the stage before starting the imaging run
+        self.logger.info("Homing the stage before starting the imaging run")
+        self.camera_controller.autofocus(False)  # Ensure autofocus is off before homing
+        self.stage_controller.move_x(0, self.app_config.get("stage_speed", 1000))
+        self.stage_controller.move_y(0, self.app_config.get("stage_speed", 1000))
+        sleep(1)  # Allow time for the stage to stabilize
 
         for sample in self.experiment.sample:
 
             self.logger.info(f"Processing sample {sample.id} at well ({sample.well_row}, {sample.well_column})")                
+            #Switch off autofocus and drop z position by 1mm before moving stage
+            self.focus_controller.autofocus(False)
+            self.focus_controller.set_focus_position(sample.focus_position - 1000)  # Drop Z
+
 
             for site_number in range(self.image_set.number_of_sites):
 
